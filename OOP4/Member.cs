89ba9b1a657e7ddd3aec534f4f38454f58a38391
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-06 21:51:17 -0600

    Last commit to OOP Basics

---
+++ OOP4/Member.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace OOP4
+{
+    public class Member
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary;
+
+        public Member(string fName, string lName, int age, double initialSalary)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+            this.salary = initialSalary;
+        }
+        
+        /* Deconstruction / Finalizer only to be created when you want to do something as the 
+            object is destroyed */
+        ~Member()
+        {
+            // Clean out statements
+            Console.WriteLine("Deconstruction of Member object...");
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    Console.WriteLine("First name cannot be an empty string.");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Last name cannot be an empty string");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = (value > 0) ? value : -value;
+        }
+
+        public double Salary => salary;
+
+        public void SelfIntroduction(bool isFriend)
+        {
+            if (isFriend) SharingPrivateInfo();
+            else
+            {
+                Console.WriteLine($"Hello, I'm {FirstName} {LastName}. I'm {Age} years old.");
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine($"Hi, dude... Well, my salary is {Salary:c2}");
+        }
+    }
+}
---
+++ OOP4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Member newMember = new Member("Julián", "Chávez", 23, 11_500);
+            Member oldMember = new Member("Israel", "Mendoza", 27, 23_400);
+            newMember.SelfIntroduction(false);
+            oldMember.SelfIntroduction(true);
+            Console.WriteLine("Creating and destroying a Member object: ");
+            TestDeconstructor();
+            Console.WriteLine("At this point, the last member should have been destroyed!!!");
+            Console.Read();
+
+        }
+
+        static void TestDeconstructor()
+        {
+            Member testMember = new Member("Otaku", "Apestoso", 11, 0);
+            testMember.SelfIntroduction(true);
+        }
+    }
+}
